0a1ec88011d1af2f15df9832f05b572a296c61ca
yavuzyagis <m.yavuzyagis@gmail.com>
2022-10-17 16:32:50 +0300

    Fix some bugs and add some features. This is an AUTOMATED Commit

--- JubaInterpreter/interpreter.java
+++ JubaInterpreter/interpreter.java
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Juba {
-    public static void main(String[] args) throws IOException}{
+    public static void main(String[] args) throws IOException{
         if (args.length > 1) {
             System.out.println("Usage: juba [script]");
             System.exit(64);
